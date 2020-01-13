928ac18cec6fee3b111cc71bae90a59d69c4c3fa
Wei Wang <weiwang2702@gmail.com>
2020-01-13 20:22:46 +0100

    init

---
+++ src/test/java/com/demo/postcode/PostcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.postcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
